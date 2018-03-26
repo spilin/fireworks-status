07a31add7ec5140cff1dd0fffc6a3284cbccd899
Alex Krasynskyi <lyoshakr@gmail.com>
2018-03-26 13:03:58 +0300

    health

---
+++ app/controllers/health_controller.rb
@@ -0,0 +1,7 @@
+class HealthController < ApplicationController
+
+  def index
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :entries
 
   root to: 'entries#index'
+  get '/health' => 'health#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
